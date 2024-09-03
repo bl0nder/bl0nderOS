7257d48b451f48d2de29d6fc2000531d45757a13
Palaash <goelpalaash@gmail.com>
2024-09-04 01:22:07 +0530

    Defined some datatypes for quality of life

---
+++ utils/data_types.h
@@ -0,0 +1,12 @@
+#ifndef DATA_TYPES_H
+#define DATA_TYPES_H
+
+typedef unsigned int u32;
+typedef unsigned short int u16;
+typedef unsigned char u8;
+
+typedef int s32;
+typedef short int s16;
+typedef char s8;
+
+#endif
